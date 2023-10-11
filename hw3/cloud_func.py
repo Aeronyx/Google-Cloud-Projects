cb5fe27fce364d04549b75792e793c101ae6bb13
George Trammell <gtram@bu.edu>
2023-10-10 21:30:40 -0400

    hw3

---
+++ hw3/cloud_func.py
@@ -0,0 +1,44 @@
+import functions_framework
+from google.cloud import storage
+from google.cloud import pubsub_v1
+import logging
+
+@functions_framework.http
+def serve_file(request):
+
+    if request.method != 'GET':
+        return ('Not yet implemented.', 501)
+
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_name = 'projects/robust-builder-398218/topics/ds561-hw3-banned-requests-sub'
+    response = subscriber.pull(subscription_name, max_messages=10)
+    if response != None:
+        print(response)
+
+    for received_message in response.received_messages:
+        print(received_message.message.data)
+        received_message.ack()
+
+    # Assuming the path format is /bucketname/username/filename
+    parts = request.path.strip('/').split('/')
+    if len(parts) < 3:
+        return ('Not Found', 404)
+
+    bucket_name = parts[0]
+    username = parts[1]
+    file_name = parts[2]
+
+    headers = ["X-country", "X-client-IP", "X-gender", "X-age", "X-income", "X-time"]
+    for header in headers:
+        logging.info(f"{header}: {request.headers.get(header, 'N/A')}")
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+
+    blob = bucket.blob(f"{username}/{file_name}")
+    if not blob.exists():
+        logging.error(f"File not found: {username}/{file_name} in bucket {bucket_name}")
+        return ('Not Found', 404)
+
+    file_content = blob.download_as_text()
+    return (file_content, 200)
---
+++ hw3/local-pubsub.py
@@ -0,0 +1,22 @@
+import os
+from google.cloud import pubsub_v1
+
+banned_countries = ['North Korea', 'Iran', 'Cuba', 'Myanmar', 'Iraq', 'Libya', 'Sudan', 'Zimbabwe'
+and 'Syria'] 
+
+publisher = pubsub_v1.PublisherClient()
+topic_name = 'projects/robust-builder-398218/topics/ds561-hw3-banned-requests'
+
+def handle_request(country):
+  if country in banned_countries:
+    data = f'Forbidden request from {country}'
+    data = data.encode('utf-8')
+    future = publisher.publish(topic_name, data)
+    print(future.result())
+    return 'Access denied', 400
+  else:
+    return 'Ok'
+
+# Test locally 
+print(handle_request('North Korea'))
+print(handle_request('United States'))
